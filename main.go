855fc1afe4af10ef3f1deed3fced0c8806265b33
rfinochi <rfinochi@gmail.com>
2019-09-07 16:43:24 -0300

    update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/rfinochi/golang-workshop-todo/docs"
+	//"github.com/rfinochi/golang-workshop-todo/docs"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
