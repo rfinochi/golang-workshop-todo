0a5d2a8bf8fbbf561c824d367c3c3e95f739638e
Rodolfo Finochietti <rfinochi@gmail.com>
2019-09-13 16:07:18 -0300

    Update main.go

--- main.go
+++ main.go
@@ -36,8 +36,11 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+		port := os.Getenv(""HTTP\_PLATFORM\_PORT"")
+		if port == "" {
+			port = "8080"
+			log.Printf("Defaulting to port %s", port)
+		}
 	}
 
 	router.Run(fmt.Sprintf(":%s", port))
