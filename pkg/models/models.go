d048a17d962da3f503ffd5df7e3086fd83c9e11d
Rodolfo Finochietti <rfinochi@gmail.com>
2020-05-12 12:22:20 -0300

    Update models.go

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,7 +6,7 @@ import (
 
 // Item godoc
 type Item struct {
-	ID     int    `json:"id,omitempty bson:"id,omitempty" datastore:"id"`
+	ID     int    `json:"id,omitempty" bson:"id,omitempty" datastore:"id"`
 	Title  string `json:"title,omitempty" bson:"title,omitempty" datastore:"title"`
 	IsDone bool   `json:"isdone,omitempty" bson:"isdone,omitempty" datastore:"isdone"`
 }
