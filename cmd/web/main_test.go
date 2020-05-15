a5b1ab31792be063e6b85315f5caab499d01897a
Rodolfo Finochietti <rfinochi@gmail.com>
2020-05-14 22:04:10 -0300

    Increase code coverage

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	"os"
-	"testing"
-)
+import "testing"
 
 func TestMain(t *testing.T) {
-	os.Setenv("PORT", "82")
-	os.Setenv("TODO_REPOSITORY_TYPE", "Memory")
-
 	go main()
 }
