d19289919df6144187bff1ff77e3e5c20ce49f83
Rodolfo Finochietti <rfinochi@gmail.com>
2020-05-15 21:15:41 -0300

    Update main_test.go

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -10,7 +10,6 @@ import (
 func TestMain(t *testing.T) {
 	os.Setenv(common.RepositoryEnvVarName, common.RepositoryMemory)
 	os.Setenv(common.PortEnvVarName, "")
-	os.Setenv(common.APITokenEnvVarName, "")
 
 	go main()
 }
